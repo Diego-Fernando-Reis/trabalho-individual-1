0088ec98f15d6bced44c17234cda86e1b6b3815d
unknown <diegoreis.dvlopr@gmail.com>
2023-01-09 22:13:56 -0300

    subindo

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
   function(){
     'use strict';
     let item = document.querySelectorAll('.timeline li');
+    let frontenders = document.getElementById('frontenders')
+    let decifrando = document.getElementById('decifrando')
+    let rodrigues = document.getElementById('rodrigues')
+    let prompt = document.getElementById('prompt')
 
     function isElementInViewport(el){
       var rect = el.getBoundingClientRect();
@@ -25,5 +29,25 @@
     window.addEventListener('load', callbackFunk)
     window.addEventListener('resize', callbackFunk)
     window.addEventListener('scroll', callbackFunk)
+
+    decifrando.addEventListener('click', ()=>{
+      prompt.value = 'https://diego-fernando-reis.github.io/projetoindividual-decifrando';
+      document.getElementById('one').style.display = 'none';
+      document.getElementById('two').style.display = 'block';
+      document.getElementById('three').style.display = 'none';
+      document.getElementById('linha').style.display = 'none';
+      document.getElementById('linha2').style.display = 'none';
+      document.getElementById('decifrando').style.padding = '0 30px';
+      document.getElementById('rodrigues').style.padding = '0 30px';
+      frontenders.style.backgroundColor = 'rgb(36, 36, 36)';
+      decifrando.style.backgroundColor = 'rgb(20, 20, 20)';
+    })
+
+    window.addEventListener('scroll', (e)=>{
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    }, false)
   }
 )()
